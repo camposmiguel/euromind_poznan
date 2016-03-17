ed7c5586ab49b6feeb82858aefc2c9b4a7f46630
Miguel Campos <camposmiguel@gmail.com>
2016-03-17 19:35:41 +0100

    DUCK

--- 02_Menus/02_NavigationDrawer/app/src/main/java/com/miguelcr/navigationdrawer/GameFragment.java
+++ 02_Menus/02_NavigationDrawer/app/src/main/java/com/miguelcr/navigationdrawer/GameFragment.java
@@ -1,29 +1,97 @@
 package com.miguelcr.navigationdrawer;
 
 
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GameFragment extends Fragment {
+public class GameFragment extends Fragment implements View.OnClickListener {
+    ImageView duck;
+    SoundPool soundPool;
+    int idSoundDuck;
+    RelativeLayout layout;
+    Random random;
+    int maxHeight, maxWidth;
+    boolean mMeasured = false;
 
 
     public GameFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        View v = inflater.inflate(R.layout.fragment_game, container, false);
+
+        v.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (!mMeasured) {
+                    // Here your view is already layed out and measured for the first time
+                    mMeasured = true; // Some optional flag to mark, that we already got the sizes
+                    // Width and height of layout
+                    maxHeight = layout.getHeight()-duck.getHeight();
+                    maxWidth = layout.getWidth()-duck.getWidth();
+
+                    generateDuckRandomPosition();
+                }
+            }
+        });
+
+        random = new Random();
+
+        layout = (RelativeLayout) v.findViewById(R.id.layout);
+        duck = (ImageView) v.findViewById(R.id.duck);
+        duck.setOnClickListener(this);
+
+
+        // Player properties
+        AudioAttributes aa = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(10)
+                .setAudioAttributes(aa)
+                .build();
+
+        // Load sound of guitar
+        idSoundDuck = soundPool.load(getActivity(),R.raw.duck_sound,1);
+
+        return v;
     }
+
+    private void generateDuckRandomPosition() {
+        // Random position
+        int positionY = random.nextInt(maxHeight - 0 + 1) + 0;
+        int positionX = random.nextInt(maxWidth - 0 + 1) + 0;
+
+        duck.setX(positionX);
+        duck.setY(positionY);
+    }
+
+    @Override
+    public void onClick(View v) {
+        soundPool.play(idSoundDuck,1,1,0,0,1);
+        generateDuckRandomPosition();
+    }
+
+
 
 }
